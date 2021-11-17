3d4b42f30976e4db3f7d4f4a51dce98f1d1ec376
Gabriel Willian <obielwb@gmail.com>
2021-11-17 09:43:05 -0300

    set server port

--- src/com/company/Servidor.java
+++ src/com/company/Servidor.java
@@ -1,7 +1,18 @@
 package com.company;
 
 public class Servidor {
+  public static String PORTA_PADRAO = "3000";
   public static void main(String[] args) {
+    if (args.length > 1) {
+      System.err.println("Uso esperado: Java Servidor [PORTA]\n");
+      return;
+    }
+
+    String porta = Servidor.PORTA_PADRAO;
+
+    if (args.length == 1)
+      porta = args[0];
+    
 
   } 
 }
