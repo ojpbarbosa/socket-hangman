3305c451f833f0f1216346f1e78e4937d27b5a16
Gabriel Willian <obielwb@gmail.com>
2021-11-22 09:28:28 -0300

    change project structure

---
+++ Cliente/Cliente.java
@@ -0,0 +1,153 @@
+package com.company;
+
+import java.net.*;
+import java.util.concurrent.ExecutionException;
+import java.io.*;
+
+public class Cliente {
+  public static final String HOST_PADRAO = "localhost";
+  public static final int PORTA_PADRAO = 3000;
+
+  public static void main(String[] args) {
+    if (args.length > 2) {
+      System.err.println("Uso esperado: Java Cliente [HOST [PORTA]]\n");
+      return;
+    }
+
+    Socket conexao = null;
+
+    try {
+      String host = Cliente.HOST_PADRAO;
+      int porta = Cliente.PORTA_PADRAO;
+
+      if (args.length > 0)
+        host = args[0];
+
+      if (args.length == 2)
+        porta = Integer.parseInt(args[1]);
+    } catch (Exception erro) {
+      System.err.println("Indique o servidor e a porta corretos!\n");
+      return;
+    }
+
+    ObjectOutputStream transmissor = null;
+
+    try {
+      transmissor = new ObjectOutputStream(conexao.getOutputStream());
+    } catch (Exception erro) {
+      System.err.println("Indique o servidor e a porta corretos!\n");
+      return;
+    }
+
+    ObjectInputStream receptor = null;
+
+    try {
+      receptor = new ObjectInputStream(conexao.getInputStream());
+    } catch (Exception erro) {
+      System.err.println("Indique o servidor e a porta corretos!\n");
+      return;
+    }
+
+    Parceiro servidor = null;
+
+    try {
+      servidor = new Parceiro(conexao, receptor, transmissor);
+    } catch (Exception erro) {
+      System.err.println("Indique o servidor e a porta corretor!\n");
+      return;
+    }
+
+    TratadoraDeComunicadoDeDesligamento tratadoraDeComunicadoDeDesligamento = null;
+    try {
+      tratadoraDeComunicadoDeDesligamento = new TratadoraDeComunicadoDeDesligamento(servidor);
+    } catch (Exception erro) {
+    } // sei que servidor foi instanciado
+
+    tratadoraDeComunicadoDeDesligamento.start();
+
+    char continuar = ' ';
+
+    do {
+      Palavra palavraSorteada = BancoDePalavras.getPalavraSorteada();
+
+      Tracinhos tracinhos = null;
+      try {
+        tracinhos = new Tracinhos(palavraSorteada.getTamanho());
+      } catch (Exception erro) {
+      }
+
+      ControladorDeLetrasJaDigitadas controladorDeLetrasJaDigitadas = new ControladorDeLetrasJaDigitadas();
+
+      ControladorDeErros controladorDeErros = null;
+      try {
+        controladorDeErros = new ControladorDeErros((int) (palavraSorteada.getTamanho() * 0.6));
+      } catch (Exception erro) {
+      }
+
+      while (tracinhos.isAindaComTracinhos() && !controladorDeErros.isAtingidoMaximoDeErros()) {
+        System.out.println("Palavra...: " + tracinhos);
+        System.out.println("Digitadas.: " + controladorDeLetrasJaDigitadas);
+        System.out.println("Erros.....: " + controladorDeErros);
+
+        try {
+          System.out.println("Sua vez de jogar, o que deseja fazer: adivinhar a palavra do jogo [1] ou digitar uma letra [2]");
+          System.out.print("Escolha um número: ");
+          int opcao = Teclado.getUmInt();
+
+          if (opcao == 1) {
+            System.out.print("Qual é a palavra ? ");
+            Palavra palavraAdivinhada = new Palavra(Teclado.getUmString().toUpperCase());
+
+            if (palavraSorteada.compareTo(palavraAdivinhada) == 0){
+              System.out.println();
+            }
+          }
+          else if (opcao == 2) {
+            System.out.print("Qual letra? ");
+            char letra = Character.toUpperCase(Teclado.getUmChar());
+  
+            if (controladorDeLetrasJaDigitadas.isJaDigitada(letra))
+              System.err.println("Essa letra ja foi digitada!\n");
+            else {
+              controladorDeLetrasJaDigitadas.registre(letra);
+  
+              int qtd = palavraSorteada.getQuantidade(letra);
+  
+              if (qtd == 0) {
+                System.err.println("A palavra nao tem essa letra!\n");
+                controladorDeErros.registreUmErro();
+              } else {
+                for (int i = 0; i < qtd; i++) {
+                  int posicao = palavraSorteada.getPosicaoDaIezimaOcorrencia(i, letra);
+                  tracinhos.revele(posicao, letra);
+                }
+                System.out.println();
+              }
+            }
+          }
+
+        } catch (Exception erro) {
+          System.err.println(erro.getMessage());
+        }
+      }
+
+      if (controladorDeErros.isAtingidoMaximoDeErros())
+        System.out.println("Que pena! Voce perdeu! A palavra era " + palavraSorteada + "\n");
+      else // !tracinhos.isAindaComTracinhos()
+        System.out.println("Parabens! Voce ganhou! A palavra era mesmo " + palavraSorteada + "\n");
+
+      for (;;) {
+        try {
+          System.out.print("Deseja jogar de novo (S/N)? ");
+          continuar = Character.toUpperCase(Teclado.getUmChar());
+          if (continuar != 'S' && continuar != 'N')
+            System.err.println("Opcao invalida! Tente novamente...");
+          else
+            break;
+        } catch (Exception erro) {
+          System.err.println("Opcao invalida! Tente novamente...");
+        }
+      }
+    } while (continuar == 'S');
+  }
+}
---
+++ Servidor/Comunicado.java
@@ -0,0 +1,7 @@
+package com.company;
+
+import java.io.*;
+
+public class Comunicado implements Serializable, Cloneable {
+  
+}
