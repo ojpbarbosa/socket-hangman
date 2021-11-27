1ab9ac8e08794184d4761ba97cb74bd75cba1db4
oJPBarbosa <contato.ferb@gmail.com>
2021-11-27 16:58:16 -0300

    add Resultado.java

---
+++ Servidor/Resultado.java
@@ -0,0 +1,11 @@
+public class Resultado {
+  private String ganhador;
+
+  public Resultado(String ganhador) {
+    this.ganhador = ganhador;
+  }
+
+  public String getGanhador() {
+    return this.ganhador;
+  }
+}
