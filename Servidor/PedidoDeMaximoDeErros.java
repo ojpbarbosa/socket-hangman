1a7deca29aa02cae818ad1e20b50c7cfebe34dc0
oJPBarbosa <contato.ferb@gmail.com>
2021-11-27 16:53:22 -0300

    add Pedidos

---
+++ Servidor/PedidoDeMaximoDeErros.java
@@ -0,0 +1,11 @@
+public class PedidoDeMaximoDeErros extends Comunicado {
+  private boolean isAtingidoMaximoDeErros; 
+
+  public void setAtingidoMaximoDeErros(boolean isAtingidoMaximoDeErros) {
+    this.isAtingidoMaximoDeErros = isAtingidoMaximoDeErros;
+  }
+
+  public boolean getIsAtingidoMaximoDeErross() {
+    return this.isAtingidoMaximoDeErros;
+  }
+}
