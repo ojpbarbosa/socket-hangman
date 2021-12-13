1e1e60552d3aabfb91ca9b3f0550c1fc88d5fedb
Vazqual <92759541+Vazqual@users.noreply.github.com>
2021-12-13 17:10:22 -0300

    Update PedidoDeMaximoDeErros.java

--- src/com/company/PedidoDeMaximoDeErros.java
+++ src/com/company/PedidoDeMaximoDeErros.java
@@ -1,17 +1,4 @@
 package com.company;
 
 public class PedidoDeMaximoDeErros extends Comunicado {
-  private boolean isAtingidoMaximoDeErros;
-
-  public PedidoDeMaximoDeErros() {
-    this.isAtingidoMaximoDeErros = false;
-  }
-
-  public boolean isAtingidoMaximoDeErros() {
-    return this.isAtingidoMaximoDeErros;
-  }
-
-  public void setIsAtingidoMaximoDeErros(boolean isAtingidoMaximoDeErros) {
-    this.isAtingidoMaximoDeErros = isAtingidoMaximoDeErros;
-  }
 }
